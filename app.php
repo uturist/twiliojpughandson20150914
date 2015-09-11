c010c9f18bc3890c0083d3ec8304d7ae61cc572a
uturist <sato_eiki@yahoo.co.jp>
2015-09-11 22:16:16 +0900

    Update app.php

--- app.php
+++ app.php
@@ -1,11 +1,10 @@
 <?php
-    
+    $app2url ="http://xxx.mybluemix.net/callcenter";
     $people = array(
                     "+8190xxxxxxxx"=>”さとう”,
                     "+8180xxxxxxxx"=>”すずき”,
                     "+8150xxxxxxxx"=>”たなか”
                     );
-    
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
@@ -13,17 +12,11 @@
 <Response>
 <?php if(!$name = $people[$_REQUEST['From']]) { ?>
     <Say language="ja-jp" voice="woman">お電話ありがとうございます。こちらはトゥイリオシステムです。担当におつなぎいたしますので、ご用件をお話しいただき、しばしそのままでお待ち下さい。</Say>
-    <Record
-    　action="http://xxx.mybluemix.net/callcenter" method="POST"
-    　maxLength="60"
-    />
+    <Record action="<?php echo $app2url ?>" method="POST" maxLength="60" />
     <Say language="ja-jp" voice="woman">申し訳ありません。もう一度お掛け直しください</Say>
 <?php } else { ?>
     <Say language="ja-jp" voice="woman"><?php echo $name ?>さんこんにちは。どうなさいましたか。</Say>
-    <Record
-    　action="http://xxx.mybluemix.net/callcenter" method="POST"
-    　maxLength="5"
-    />
+    <Record action="<?php echo $app2url ?>" method="POST" maxLength="5" />
     <Say language="ja-jp" voice="woman"><?php echo $name ?>さん申し訳ありません。もう一度お掛け直しください</Say>
 <?php } ?>
 </Response>
